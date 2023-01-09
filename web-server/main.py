d7185984d6c2eda7faeb52afd2cea3b8fd55062e
Pablo Gallegos <pabli1128@hotmail.com>
2023-01-09 16:14:11 -0500

    Modified docks, Add docs: Dockfile and docker-compose

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI() # Crear instancia
+
+@app.get("/") #Ruta por la cual desde la web podrá ingresar
+def get_list():
+    return [1,2,3]
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>I'm a page</h1>
+        <p>I'm a paragraph</p>
+    """
 
 def run():
     store.get_categories()
